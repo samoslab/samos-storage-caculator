434b82fa6f01f8412a85b4e54f196d4bc1775f0d
yuting17 <yuting17@staff.sina.com.cn>
2018-07-10 19:18:21 +0800

    fix the router

--- router/router.go
+++ router/router.go
@@ -3,21 +3,21 @@ package router
 import (
 	"os"
 	"net/http"
+	"path/filepath"
 	"github.com/samoslab/samos-storage-caculator/action"
-	"path"
 )
 
 type Router struct {
 }
 
 func (router *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
-	if r.URL.Path == "" {
+	if r.URL.Path == "/" {
 		action.IndexAction(w, r)
 	} else if r.URL.Path == "/cal" {
 		action.CalAction(w, r)
-	} else if r.URL.Path == "/static" {
-		dir := path.Dir(os.Args[0])
+	} else {
+		dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+		dir = dir + "/public"
 		staticHandler := http.FileServer(http.Dir(dir))
 		staticHandler.ServeHTTP(w, r)
 	}
